9bf35c71173be4bd623a6bd35c00994c01fed019
Dev-PEB <118755228+Dev-PEB@users.noreply.github.com>
2023-11-18 12:20:45 +0100

    Update mynetwork.tf

--- mynetwork.tf
+++ mynetwork.tf
@@ -1,7 +1,7 @@
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
   name    = "mynetwork"
-  project = "kayprjct03-353511"
+  project = "playground-s-11-2ab6fb0a 	"
   #RESOURCE properties go here
   auto_create_subnetworks = "true"
 }
@@ -9,7 +9,7 @@ resource "google_compute_network" "mynetwork" {
 # Add a firewall rule to allow HTTP, SSH, RDP, and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
   name    = "mynetwork-allow-http-ssh-rdp-icmp"
-  project = "kayprjct03-353511"
+  project = "playground-s-11-2ab6fb0a 	"
   network = google_compute_network.mynetwork.self_link
   allow {
     protocol = "tcp"
@@ -22,13 +22,6 @@ resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
 }
 
 
-
-# Create the mynet-us-vm instance
-module "mynet-us-vm" {
-  source           = "./instance"
-  instance_name    = "mynet-us-vm"
-  instance_zone    = "us-central1-a"
-  instance_network = google_compute_network.mynetwork.self_link
 }
 # Create the mynet-eu-vm" instance
 module "mynet-eu-vm" {
