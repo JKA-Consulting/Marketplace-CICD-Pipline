560771868b606b04558f82432d545ba7e2552ce8
Abasi-ifreke Uwem <uwemabasifrekelive@gmail.com>
2024-03-18 00:43:43 +0100

    changed the startup script on the main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,25 @@ resource "google_compute_instance" "jenkins-instance" {
 
   }
 
-  metadata_startup_script = "${file("/jenkinsScript.sh")}"
+  # metadata_startup_script = "${file("/jenkinsScript.sh")}"
+
+  metadata_startup_script = <<-EOT
+            #!/bin/bash
+
+            sudo apt update
+            sudo apt install fontconfig openjdk-17-jre -y
+            java -version
+            sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+              https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+            echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+              https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+              /etc/apt/sources.list.d/jenkins.list > /dev/null
+            sudo apt-get update
+            sudo apt-get install jenkins
+            sudo systemctl enable jenkins
+            sudo systemctl start jenkins
+            sudo systemctl status jenkins
+        EOT
 }
 
 resource "google_compute_firewall" "allow-ssh-rdp-icmp" {
